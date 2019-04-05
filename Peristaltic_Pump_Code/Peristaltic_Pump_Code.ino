bb9287b03b01ea35622043af6cd0d02fbc034a86
TommyStyrvoky <Tommystyrvoky@gmail.com>
2019-04-05 13:17:32 -0500

    Update Peristaltic_Pump_Code.ino

--- Peristaltic_Pump_Code/Peristaltic_Pump_Code.ino
+++ Peristaltic_Pump_Code/Peristaltic_Pump_Code.ino
@@ -3,6 +3,7 @@
 #include <Bounce2.h>// https://github.com/thomasfredericks/Bounce2
 Bounce debouncer = Bounce();
 #define BUTTON 4
+#include <EEPROM.h>
 #include<LiquidCrystal.h>
 LiquidCrystal lcd(12, 11, 10, 9, 8, 7);
 #include <ClickEncoder.h>//https://github.com/0xPIT/encoder/tree/arduino
@@ -10,19 +11,19 @@ ClickEncoder encoder(6, 5, 4, 4);
 int16_t oldEncPos, encPos;
 const int STEP = 3;//Stepper controller step
 const int DIR = 2;//Stepper controller direction
-String Menu[] = {"Pump mode", "Flow", "Direction", "Volume", "Deliver", "Calibration vol.", "Blowout Volume"};
+String Menu[] = {"Pump mode", "Flow", "Direction", "Volume", "Deliver", "Calibration vol.", "Blowout volume"};
 String Pumpmode[] = {"Off             ", "Continouous       ", "Volumetric        "};
 String SerialData;
-int MenuValue = 0, PumpMode = 0, i = 0;
+int MenuValue = 0, PumpMode = 0;
 boolean ButtonValue = LOW, Direction = LOW;
 int StepsPermL = 2080;// internal value for calibration of the pump
-int MinStepTime = 600;//minimum step duration
+int MinStepTime = 400;//minimum step duration
 int ms = MinStepTime;// time in microseconds
 int TimePerHStep = 0;//time in milliseconds per full step
-double VolumeToDeliver;//volume to deliver in volumetric mode
+double VolumeToDeliver;//volume to deliver in pump mode 3
 int VolumeToDeliverInt;
 int Blowoutint = 15;
-double Blowout = Blowoutint / 100.00;
+double Blowout = 0.15;
 boolean Deliver = LOW;
 
 void setup() {
@@ -44,6 +45,7 @@ void loop() {
   encPos += encoder.getValue();
   if (Serial.available() > 0) {
     SerialInterface();
+    LCDMenu();
   }
   ButtonMode();
   while (encPos != oldEncPos || ButtonValue == HIGH) {
@@ -54,45 +56,15 @@ void loop() {
   }
   Pump();
 }
-void SerialInterface() { // Serial data interface with pump
-  if (i > 5) {
-    i = 0;
-  }
+void SerialInterface() {
   SerialData = Serial.readString();    // read the incoming data
-  SerialData.remove((SerialData.length() - 1));
-  switch (i) {//assign to pump setting
-    case 0:
-      Serial.println("PumpMode");
-      PumpMode = SerialData.toInt();
-      LCDMenu();
-      break;
-    case 1:
-      Serial.println("ms");
-      ms = SerialData.toInt();//ms per step
-      LCDMenu();
-      break;
-    case 2:
-      Serial.println("µs");
-      TimePerHStep = SerialData.toInt(); //µs per step
-      LCDMenu();
-      break;
-    case 3:
-      Serial.println("Direction");
-      Direction = SerialData.toInt();//0 or 1
-      LCDMenu();
-      break;
-    case 4:
-      Serial.println("VolumeToDeliver");
-      VolumeToDeliver = SerialData.toDouble();//0.00-1000.00
-      LCDMenu();
-      break;
-    case 5:
-      Serial.println("Deliver");
-      Deliver = SerialData.toInt();// 0 or 1
-      LCDMenu();
-      break;
-  }
-  i++;
+  PumpMode = SerialData.substring(0, 1).toInt();
+  ms = SerialData.substring(1, 4).toInt();
+  TimePerHStep = SerialData.substring(4, 6).toInt();
+  Direction = SerialData.substring(6, 7).toInt();
+  digitalWrite(DIR, Direction);
+  VolumeToDeliver = SerialData.substring(7, 14).toDouble();
+  Deliver = SerialData.substring(14, 15).toInt();
 }
 void ButtonMode() {
   if (digitalRead(BUTTON) == LOW) {
@@ -100,7 +72,7 @@ void ButtonMode() {
     delay(250);
   }
 }
-void LCDMenu() {//LCD menu top line
+void LCDMenu() {//menu
   lcd.clear();
   MenuValue = constrain(MenuValue , 0 , 6);
   if (ButtonValue == LOW) {
@@ -119,10 +91,10 @@ void LCDMenu() {//LCD menu top line
   lcd.setCursor(0, 1);
   LCDbottom();
 }
-void LCDbottom() { //LCD menu bottom line
+void LCDbottom() {
   ms = constrain(ms, 0, 1000);
   VolumeToDeliver = constrain(VolumeToDeliver, 0.00 , 1000.00);
-  double Flow = ((60000.000 / 2) / (TimePerHStep + (ms / 1000.00)))  / StepsPermL; //flowrate of the pump in mL/min
+  double Flow = ((60000.00/2) / (TimePerHStep + (ms / 1000.00)))  / StepsPermL; //flowrate of the pump in mL/min
   Flow = constrain(Flow, 0, 25);
   switch (MenuValue) {
     case 0:
@@ -155,7 +127,7 @@ void LCDbottom() { //LCD menu bottom line
       lcd.print("mL/min");
       break;
     case 2:
-      if (Direction == LOW) {
+      if (Direction == LOW) { // Direction
         lcd.print("CW");
       }
       else {
@@ -171,7 +143,7 @@ void LCDbottom() { //LCD menu bottom line
         }
         break;
       case 3:
-        if (ButtonValue == HIGH) {//  volume to deliver
+        if (ButtonValue == HIGH) {// volume to deliver
           VolumeToDeliverInt += 5 * encoder.getValue();
           VolumeToDeliver = VolumeToDeliverInt / 100.00;
           if (VolumeToDeliver < 0) {
@@ -197,49 +169,50 @@ void LCDbottom() { //LCD menu bottom line
         }
         break;
       case 5:
-        if (ButtonValue == HIGH) {//StepsPermL
+        if (ButtonValue == HIGH) {// StepsPermL
           StepsPermL += encoder.getValue();
         }
+        //EEPROM.write(StepsPermL, 1);
         lcd.print(StepsPermL);
-        lcd.print("Steps");
+        lcd.print("mL");
         break;
       case 6:
         if (ButtonValue == HIGH) {// Blowoutvolume
           Blowoutint += encoder.getValue();
+          //EEPROM.write(Blowoutint, 2);
           Blowout = Blowoutint / 100.00;
         }
-        Serial.print(Blowout);
         lcd.print(Blowout);
         lcd.print("mL");
         break;
       }
+
   }
 }
-void Pump() {// Pump mode
+void Pump() {// Pump settings
   int BlowoutVolume = Blowout * StepsPermL; //Blowout volume
+  double Flow = (60000.00 / (TimePerHStep + (ms / 1000.00)))  / StepsPermL; //flowrate of the pump in mL/min
   long Totali = VolumeToDeliver * StepsPermL; // number of steps needed for pump to deliver specified volume
   if (PumpMode == 1) {// Continuous volume delivery mode
     Step();
   }
-  if (PumpMode == 2) {
-    if (Deliver == HIGH) {// Specific volume delivery mode
-      for (int i = 0; i < (Totali + BlowoutVolume); i++) {//Volume delivery, and first part of blowout
-        digitalWrite(STEP, HIGH);
-        Step();
-      }
-      for (int i = 0; i < BlowoutVolume; i++) { //reverses directon to counter added blowout volume when done
-        digitalWrite(DIR, !Direction);
-        Step();
-      }
-      digitalWrite(DIR, Direction);
-      Deliver = LOW;
-      LCDMenu();
+  if (PumpMode == 2 && Deliver == HIGH) { // Specific volume delivery mode
+    for (int i = 0; i < (Totali + BlowoutVolume); i++) {//Volume delivery, and first part of blowout
+      digitalWrite(STEP, HIGH);
+      Step();
     }
+    for (int i = 0; i < BlowoutVolume; i++) { //reverses directon to counter added blowout volume when done
+      digitalWrite(DIR, !Direction);
+      Step();
+    }
+    digitalWrite(DIR, Direction);
+    Deliver = LOW;
+    LCDMenu();
   }
 }
 void Step() {//Step timing
   digitalWrite(STEP, HIGH);
-  if (ms < 600 && TimePerHStep < 1) {
+  if (ms < MinStepTime && TimePerHStep < 1) {
     ms = MinStepTime;
     delayMicroseconds(ms);
   }
@@ -248,7 +221,7 @@ void Step() {//Step timing
     delay(TimePerHStep);
   }
   digitalWrite(STEP, LOW);
-  if (ms < 600 && TimePerHStep < 1) {
+  if (ms < MinStepTime && TimePerHStep < 1) {
     ms = MinStepTime;
     delayMicroseconds(ms);
   }
